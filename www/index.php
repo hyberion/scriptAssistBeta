fa998162d16aa0e14735fc48d3d272797f0fd5ac
hyberion <hyberion@users.noreply.github.com>
2025-01-12 04:09:54 -0500

    homepage tweaks, animation added.  Title added.  Other minor initial adjustments

--- www/index.php
+++ www/index.php
@@ -1,9 +1,27 @@
 <!doctype html>
-<html><head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-<!-- New Wappler Page -->
-</head>
-<body>
+<html>
 
-</body></html>
+<head>
+    <base href="/">
+    <script src="dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>Script Assist Beta</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="bootstrap/5/cyborg/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/custom.css" />
+    <link rel="stylesheet" href="dmxAppConnect/dmxAnimateCSS/animate.min.css" />
+    <script src="dmxAppConnect/dmxAnimateCSS/dmxAnimateCSS.js" defer></script>
+</head>
+
+<body is="dmx-app" id="index">
+    <div class="style2">
+        <h1 dmx-animate-enter.5.5="fadeInDown">Script Assist Beta</h1>
+    </div>
+
+    <script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
